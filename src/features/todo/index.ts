640f09d6cb20fbfc307978b0445a7ab3e7369a42
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-05-07 22:12:53 +0330

    feat: export todo-delete

--- src/features/todo/index.ts
+++ src/features/todo/index.ts
@@ -1,7 +1,10 @@
 // api
+export * from "./api/add-task";
+// export * from "./api/delete-task";
 export * from "./api/get-tasks";
 // components
 export * from "./components/todo-add";
+export * from "./components/todo-delete";
 export * from "./components/todo-list";
 export * from "./components/todo-list-skeleton";
 export * from "./components/todo-list-toolbar";

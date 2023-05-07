21a034c3272e5d3217644b064e6bd77bf1e75a6f
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-05-07 22:13:40 +0330

    feat: add TodoDelete component

---
+++ src/features/todo/components/todo-delete.tsx
@@ -0,0 +1,33 @@
+import { Show } from "@/components/show";
+import { useState } from "react";
+import { useDeleteTask } from "../api/delete-task";
+
+type Props = {
+  id: string;
+};
+export const TodoDelete = ({ id }: Props): JSX.Element => {
+  const { onDelete } = useDeleteTask();
+  const [confirm, setConfirm] = useState<boolean>(false);
+  const onToggleConfirm = () => {
+    setConfirm((confirm) => !confirm);
+  };
+  return (
+    <section className="flex">
+      <Show when={!confirm}>
+        <button className="btn btn-xs" onClick={onToggleConfirm}>
+          Delete
+        </button>
+      </Show>
+      <Show when={confirm}>
+        <div className="flex">
+          <button className="btn btn-xs btn-error" onClick={() => onDelete(id)}>
+            Delete
+          </button>
+          <button className="btn btn-xs" onClick={onToggleConfirm}>
+            Cancel
+          </button>
+        </div>
+      </Show>
+    </section>
+  );
+};

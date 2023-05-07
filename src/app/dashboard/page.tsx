b561c311e3564491ab551d6de0426d313e2b11dd
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-05-08 01:24:03 +0330

    style: add padding

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -20,7 +20,7 @@ export default function DashboardPage() {
 
   const { data, isFetching } = useGetTasks();
   return (
-    <main className="max-w-3xl py-8 mx-auto">
+    <main className="max-w-3xl px-4 py-8 mx-auto">
       <TodoListToolbar />
       <section className="mt-2">
         {isFetching ? <TodoListSkeleton /> : <TodoList todos={data} />}

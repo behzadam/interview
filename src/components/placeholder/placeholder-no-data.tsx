0b45e5aa38d9a6c94c6d8c83492cf9e4f13b5813
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-05-07 20:24:18 +0330

    feat: add no data component

---
+++ src/components/placeholder/placeholder-no-data.tsx
@@ -0,0 +1,22 @@
+export const PlaceholderNoData = (): JSX.Element => {
+  return (
+    <div className="mt-4 text-gray-700 alert bg-slate-300">
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          className="flex-shrink-0 w-6 h-6 stroke-info"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          ></path>
+        </svg>
+        <span>No data!</span>
+      </div>
+    </div>
+  );
+};

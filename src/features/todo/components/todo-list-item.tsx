ba9b8e37fa64992794d84abfac7ffd55a23bff49
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-05-07 22:13:52 +0330

    feat: add TodoDelete component

--- src/features/todo/components/todo-list-item.tsx
+++ src/features/todo/components/todo-list-item.tsx
@@ -1,4 +1,5 @@
 import { Todo } from "../types/todo";
+import { TodoDelete } from "./todo-delete";
 
 type Props = {
   todo: Todo;
@@ -7,6 +8,9 @@ export const TodoListItem = ({ todo }: Props): JSX.Element => {
   return (
     <tr>
       <td>{todo.text}</td>
+      <td width={100}>
+        <TodoDelete id={todo.id} />
+      </td>
     </tr>
   );
 };

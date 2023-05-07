aa3273b07b9ea9cf306dbd096dceee0da7c48af0
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-05-08 00:24:03 +0330

    feat: update logo | user redirect

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -8,14 +8,12 @@ const headingFont = Abril_Fatface({
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between p-5 border-b dark:border-y-gray-800">
-      <h1
-        className={`${headingFont.className} font-bold text-2xl text-cyan-800 dark:text-gray-50`}
-      >
-        E<span className="dark:text-gray-600">nterview...</span>
+    <header className="flex items-center justify-between p-5 dark:border-b bg-slate-50 dark:border-y-gray-800">
+      <h1 className={`${headingFont.className} font-bold text-2xl`}>
+        E<span className="text-indigo-700 dark:text-gray-600">nterview</span>!
       </h1>
       <SignedIn>
-        <UserButton />
+        <UserButton afterSignOutUrl="/" />
       </SignedIn>
       <SignedOut>
         <SignInButton />

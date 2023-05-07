c157d67b671d2000211b7c27801cf807ec103780
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-05-07 20:24:36 +0330

    feat: export todo feature

---
+++ src/features/todo/index.ts
@@ -0,0 +1,9 @@
+// api
+export * from "./api/get-tasks";
+// components
+export * from "./components/todo-add";
+export * from "./components/todo-list";
+export * from "./components/todo-list-skeleton";
+export * from "./components/todo-list-toolbar";
+// types
+export * from "./types/todo";

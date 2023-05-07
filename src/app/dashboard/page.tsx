710bad8170c365d2d160214166c5a356fd008c63
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-05-08 00:25:48 +0330

    feat: add page auth guard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+
 import {
   TodoList,
   TodoListSkeleton,
@@ -8,6 +11,13 @@ import {
 } from "@/features/todo";
 
 export default function DashboardPage() {
+  const { isLoaded, isSignedIn, user } = useUser();
+  const router = useRouter();
+
+  if (isLoaded && !isSignedIn) {
+    router.push("/sign-in?redirectUrl=/dashboard");
+  }
+
   const { data, isFetching } = useGetTasks();
   return (
     <main className="max-w-3xl py-8 mx-auto">
